dc8000a194c725b3c8b2f25b49553fd97029f365
kajal bhardwaj <bhardwaj.kajal2007@gmail.com>
2026-06-02 13:32:48 +0530

    Day2 solution completed

---
+++ Week_1/Day_2/maximum_average_subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+       double currsum =0;
+       for(int i=0; i<k; i++){
+        currsum += nums[i]; 
+       }  
+       double maxsum = currsum;
+       for(int i=k; i<nums.size(); i++){
+        currsum += nums[i]-nums[i-k];
+        maxsum = max(maxsum , currsum);
+       }
+        return maxsum/k;
+    }
+};
