cfa8b14f9d514c79075807522738c368efc64867
kajal bhardwaj <bhardwaj.kajal2007@gmail.com>
2026-06-12 22:27:06 +0530

    week2 day5 solution completed

---
+++ Week_2/Day_5/string_compression.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0, idx = 0;
+        
+        while (i < chars.size()) {
+            char ch = chars[i];
+            int cnt = 0;
+            
+            while (i < chars.size() && chars[i] == ch) {
+                cnt++;
+                i++;
+            }
+            
+            chars[idx++] = ch;
+            
+            if (cnt > 1) {
+                string s = to_string(cnt);
+                for (char c : s) {
+                    chars[idx++] = c;
+                }
+            }
+        }
+        
+        return idx;
+    }
+};
